448284b2085aa1d67344f2cd6e08e218efff0dc0
Enzo Laugel <enzo.laugel@epitech.eu>
2025-10-07 21:55:19 +0200

    feat: main page now can redirect to the blackjack page

--- arcarde_client/src/App.tsx
+++ arcarde_client/src/App.tsx
@@ -1,21 +1,28 @@
 import './App.css'
+import Blackjack from './blackjack'
+import { useState } from 'react'
 
-function MyButton({ title }: { title: string }) {
+function MyButton({ title, onClick }: { title: string; onClick?: () => void }) {
   return (
-    <button>{title}</button>
+    <button onClick={onClick}>{title}</button>
   );
 }
 
 function App() {
+  const [currentGame, setCurrentGame] = useState<string | null>(null);
+
+  if (currentGame === 'blackjack') {
+    return <Blackjack />;
+  }
 
   return (
     <>
       <h1 id="brand_name">Arcade Games</h1>
       <div id="btn_game">
-        <MyButton title ="Black Jack" />
-        <MyButton title ="Snake" />
-        <MyButton title ="Russian Roulette" />
-        <MyButton title ="Slot Machine" />
+        <MyButton title="Black Jack" onClick={() => setCurrentGame('blackjack')} />
+        <MyButton title="Snake" />
+        <MyButton title="Russian Roulette" />
+        <MyButton title="Slot Machine" />
       </div>
     </>
   )
